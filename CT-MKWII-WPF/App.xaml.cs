2895709f2805d334adf7e610112bc81c76349c9e
patchzyy <64382339+patchzyy@users.noreply.github.com>
2024-05-08 11:19:46 +0200

    Add version numbering

--- CT-MKWII-WPF/App.xaml.cs
+++ CT-MKWII-WPF/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace CT_MKWII_WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
