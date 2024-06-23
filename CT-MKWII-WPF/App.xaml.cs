52bad66d6580d596fe12390f093132010969f27f
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-06-23 22:30:16 +0200

    first steps at creating the new ui base layout

--- CT-MKWII-WPF/App.xaml.cs
+++ CT-MKWII-WPF/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using CT_MKWII_WPF.Pages;
-using CT_MKWII_WPF.Utils;
+﻿using System.Windows;
 using CT_MKWII_WPF.Utils.Auto_updator;
 
 namespace CT_MKWII_WPF
---
+++ CT-MKWII-WPF/Views/Layout.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CT_MKWII_WPF.Views;
+
+public partial class Layout : Window
+{
+    public Layout()
+    {
+        InitializeComponent();
+    }
+    
+    private void TopBar_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            DragMove();
+        }
+    }
+    
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+    
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
