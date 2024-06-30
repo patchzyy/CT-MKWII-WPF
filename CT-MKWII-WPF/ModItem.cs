519f98ac938c4436f600c666b99ecc8b930ed2be
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-06-30 19:02:50 +0200

    temporary list view

--- CT-MKWII-WPF/ModItem.cs
+++ CT-MKWII-WPF/ModItem.cs
@@ -1,6 +1,8 @@
 namespace CT_MKWII_WPF;
 
+    
 public class ModItem
 {
-    
+    public string ModName { get; set; }
+    public bool IsEnabled { get; set; }
 }
--- CT-MKWII-WPF/Views/Pages/ModsPage.xaml.cs
+++ CT-MKWII-WPF/Views/Pages/ModsPage.xaml.cs
@@ -1,11 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows;
 
-namespace CT_MKWII_WPF.Views.Pages;
-
-public partial class ModsPage : Page
+namespace CT_MKWII_WPF.Views.Pages
 {
-    public ModsPage()
+    public partial class ModsPage : Page
     {
-        InitializeComponent();
+        //public ObservableCollection<ModItem> Mods { get; set; }
+
+        public ModsPage()
+        {
+            InitializeComponent();
+            
+            // // when setting in code, make sure you bind the Source like so in the XAML: ItemsSource="{Binding Mods}"
+            
+            //Mods = new ObservableCollection<ModItem>
+            //{
+            //    new ModItem { ModName = "Sample Mod 1",  IsEnabled=true },
+            //    new ModItem { ModName = "Sample Mod 2",  IsEnabled=true },
+            //    new ModItem { ModName = "Sample Mod 3",  IsEnabled=true }
+            //};
+
+            ModsListView.DataContext = this;
+        }
+        
+        
     }
 }
