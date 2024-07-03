b5648551de4c051c73896c5b58f4f98c4c227472
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-07-04 00:32:59 +0200

    step 1 of creating drag an drop

--- CT-MKWII-WPF/Views/Components/DraggableListView.xaml.cs
+++ CT-MKWII-WPF/Views/Components/DraggableListView.xaml.cs
@@ -1,16 +1,18 @@
 using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using static CT_MKWII_WPF.Views.ViewUtils;
 
 namespace CT_MKWII_WPF.Views.Components;
 
 public partial class DraggableListView : ListView
 {
-
     private ListViewItem? _listViewItem;
-
+    //private Point _startPoint;
+    
     // Public property with a private setter and a public getter
     public ListViewItem? ContextMenuListViewItem
     {
@@ -24,10 +26,19 @@ public partial class DraggableListView : ListView
         Loaded += OnLoaded;
     }
     
+    public static readonly DependencyProperty ItemContextMenuProperty = DependencyProperty.Register(
+        nameof(ItemContextMenu), typeof(ContextMenu), typeof(DraggableListView),
+        new PropertyMetadata(null));
+
+    public ContextMenu? ItemContextMenu
+    {
+        get => (ContextMenu)GetValue(ItemContextMenuProperty);
+        set => SetValue(ItemContextMenuProperty, value);
+    }
+    
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
-        if (!(View
-                is GridView gridView)) return; 
+        if (!(View is GridView gridView)) return; 
 
         // here we create an empty column that will later be filled with the icon for each row
         // We create an empty column that will be filled in order to not mess up any other columns arrangement
@@ -47,18 +58,71 @@ public partial class DraggableListView : ListView
         return ItemContainerGenerator.ItemFromContainer(_listViewItem) as T;
     }
     
-    private void DotsIcon_Click(object sender, RoutedEventArgs e)
+    private void ContextMenu_Click(object sender, RoutedEventArgs e)
     {
         _listViewItem = FindAncestor<ListViewItem>(e.OriginalSource)!;
-        if (sender is FrameworkElement && ContextMenu != null)
-            ContextMenu.IsOpen = true;
+        if (sender is FrameworkElement && ItemContextMenu != null)
+            ItemContextMenu.IsOpen = true;
     }
 
-    private void ListViewItem_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e) => 
-        _listViewItem = FindAncestor<ListViewItem>(e.OriginalSource);
-
-    private void GripIcon_Click(object sender, RoutedEventArgs e)
+    private void GripIcon_Hold(object sender, MouseButtonEventArgs e)
     {
-        Console.WriteLine("GripIcon_Click");
+        //_startPoint = e.GetPosition(this);
+        ListViewItem listViewItem = FindAncestor<ListViewItem>(e.OriginalSource)!;
+        listViewItem.Opacity = 0.4;
+        var dragData = new DataObject("listViewItem", listViewItem);
+        DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
+    }
+
+    private void GripIcon_MouseMove(object sender, MouseEventArgs e)
+    {
+    }
+
+    
+    private void GripIcon_OnDrop(object sender, DragEventArgs e)
+    {
+        if(!(e.Data.GetData("listViewItem") is ListViewItem listViewItem)) return;
+        listViewItem.Opacity = 1;
+        Point dropPosition = e.GetPosition(this);
+        int targetIndex = GetDropTargetIndex(dropPosition);
+        targetIndex = Math.Clamp(targetIndex, 0, Items.Count - 1);
+
+        Console.WriteLine(ItemsSource.GetType().BaseType);
+        
+        // the least worst solution now seems to be to create a interface something like DragableItem, then just check for that instead
+        // then we can also add attribs to that Draggable item if that is needed
+        
+        if (!(ItemsSource is ObservableCollection<ModItem> itemList))
+        {
+            Console.WriteLine(ItemsSource.GetType().IsGenericType);
+            Console.WriteLine("ERROR: This type of list is not usabled for a draggable container");
+            return;
+        }
+   
+        ModItem itemThing = (ItemContainerGenerator.ItemFromContainer(listViewItem) as ModItem)!;
+        itemList.Remove(itemThing);
+        itemList.Insert(targetIndex, itemThing);
+        
+        Console.WriteLine(targetIndex);
+    }
+    
+    private int GetDropTargetIndex(Point dropPosition)
+    {
+        for (int i = 0; i < Items.Count; i++)
+        {
+            ListViewItem listViewItem = (ListViewItem)ItemContainerGenerator.ContainerFromIndex(i);
+            if (listViewItem != null)
+            {
+                Rect itemBounds = VisualTreeHelper.GetDescendantBounds(listViewItem);
+                Point itemPosition = listViewItem.TransformToAncestor(this).Transform(new Point(0, 0));
+
+                if (dropPosition.Y < itemPosition.Y + itemBounds.Height)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
     }
 }
--- CT-MKWII-WPF/Views/Pages/ModsPage.xaml.cs
+++ CT-MKWII-WPF/Views/Pages/ModsPage.xaml.cs
@@ -1,28 +1,24 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Media;
-using static CT_MKWII_WPF.Views.ViewUtils;
+using System.Collections.ObjectModel;
 
 namespace CT_MKWII_WPF.Views.Pages
 {
     public partial class ModsPage : Page
     {
-        //public ObservableCollection<ModItem> Mods { get; set; }
+        public ObservableCollection<ModItem> Mods { get; set; }
 
         public ModsPage()
         {
             InitializeComponent();
             
-            // // when setting in code, make sure you bind the Source like so in the XAML: ItemsSource="{Binding Mods}"
-            
-            //Mods = new ObservableCollection<ModItem>
-            //{
-            //    new ModItem { ModName = "Sample Mod 1",  IsEnabled=true },
-            //    new ModItem { ModName = "Sample Mod 2",  IsEnabled=true },
-            //    new ModItem { ModName = "Sample Mod 3",  IsEnabled=true }
-            //};
+            Mods = new ObservableCollection<ModItem>
+            {
+                new ModItem { ModName = "Sample Mod 1",  IsEnabled=true },
+                new ModItem { ModName = "Sample Mod 2",  IsEnabled=true },
+                new ModItem { ModName = "Sample Mod 3",  IsEnabled=true }
+            };
 
             ModsListView.DataContext = this;
         }
@@ -30,12 +26,8 @@ namespace CT_MKWII_WPF.Views.Pages
 
         private void MenuItem_OnClick(object sender, RoutedEventArgs e)
         {
-        
             var modItem = ModsListView.GetCurrentContextItem<ModItem>();
             Console.WriteLine( modItem.ModName );
-            
         }
-        
-    
     }
 }
